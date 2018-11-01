3f5d4ee2d7f88e60b3d2ef2eb0811560d46c3518
At-furia <alexandre.1608@hotmail.fr>
2018-11-01 18:13:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,33 @@
         
           bot.login(process.env.TOKEN);
 
-      bot.on('guildMemberAdd', member => {
-        let brig = member.guild.roles.find("name", "BAT LES COUILLES FRERE");
-        member.addRole(brig);
-})
+    //  bot.on('guildMemberAdd', member => {
+       // let brig = member.guild.roles.find("name", "BAT LES COUILLES FRERE");
+       // member.addRole(brig);
+//})
+
+bot.on('message', message => {
+    let userVar = message.author
+    let pUser = message.mentions.users.first()
+
+    if (message.author.bot) return;
+    if (message.channel.type === 'dm') {
+      if(message.content.includes('@')){
+          message.reply("Impossible de répondre a votre demande si elle contient une mention")
+         } else {
+
+  //    message.delete()
+        bot.channels.get("507602511209693185").send(message.content.slice(0, message.content.length)+ " " + userVar); 
+
+    }}
+
+
+    if (message.content.startsWith(prefix + "mp")) {
+                
+        if (!pUser) {
+        }
+        else {
+            pUser.sendMessage(message.content.slice(3, message.content.length));
+    }
+    }
+    })
